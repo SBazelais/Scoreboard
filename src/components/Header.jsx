b627c9904b3153e12064ec98326ce0f20bd8335a
Steve Bazelais <57912340+SBazelais@users.noreply.github.com>
2021-08-28 08:24:59 -0400

    build stop watch ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import Stats from "./Stats";
+import Stopwatch from "./Stopwatch";
 
-export const Header = ({ title, players }) => {
+export const Header = (props) => {
+  const { title, players } = props;
   return (
     <header>
       <Stats players={players} />
       <h1>{title}</h1>
+      <Stopwatch />
     </header>
   );
 };
